1b897bca14cda798c251855680e6ce2d0dbc4344
eunchae <kec0130@gmail.com>
2023-12-06 22:12:56 +0900

    Setup project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { withContentlayer } = require('next-contentlayer');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+module.exports = withContentlayer(nextConfig);
