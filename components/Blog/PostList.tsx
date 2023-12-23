4c52499412f224c23079d0b77886fcef61baea4f
eunchae <kec0130@gmail.com>
2023-12-24 00:29:44 +0900

    Fix list item key

--- components/Blog/PostList.tsx
+++ components/Blog/PostList.tsx
@@ -4,8 +4,8 @@ import PostListItem from './PostListItem';
 const PostList = ({ posts }: { posts: Post[] }) => {
   return (
     <ul className="grid grid-cols-1 gap-x-10 gap-y-14 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-12">
-      {posts.map((post, index) => (
-        <PostListItem key={index} post={post} />
+      {posts.map((post) => (
+        <PostListItem key={post.slug} post={post} />
       ))}
     </ul>
   );
