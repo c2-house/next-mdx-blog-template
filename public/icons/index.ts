deca0c1636ce9a194238456296f831edfb7220a7
eunchae <kec0130@gmail.com>
2024-01-14 19:51:39 +0900

    Fix link preview loading svg

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,5 +1,6 @@
 import MenuIcon from './menu.svg';
 import CloseIcon from './close.svg';
 import GlobeIcon from './globe.svg';
+import LoadingIcon from './loading.svg';
 
-export { MenuIcon, CloseIcon, GlobeIcon };
+export { MenuIcon, CloseIcon, GlobeIcon, LoadingIcon };
